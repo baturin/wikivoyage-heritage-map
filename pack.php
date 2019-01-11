9345d22b557cbc0e2304243a2e2381cc13513071
Alexey Baturin <alexey.baturin.nsk@gmail.com>
2019-01-12 01:45:05 +0700

    Simple script to build dist

---
+++ pack.php
@@ -0,0 +1,22 @@
+<?php
+
+chdir(dirname(__FILE__));
+if (!file_exists('dist')) {
+    mkdir('dist');
+}
+
+$resultFilename = 'dist/wvmap.tar.gz';
+if (file_exists('dist/wvmap.tar.gz')) {
+    unlink($resultFilename);
+}
+
+$itemsToPack = [
+    'index.htm',
+    'api.php',
+    'ico24',
+    'img',
+    'lib',
+    'locale'
+];
+$itemsToPackStr = implode(' ', $itemsToPack);
+system("tar -cvf {$resultFilename} {$itemsToPackStr}");
