79fd80346ffef5a9155753ed7bc49ee40215363d
Alexey Baturin <alexey.baturin.nsk@gmail.com>
2019-03-14 01:06:33 +0700

    Set version during build

--- pack.php
+++ pack.php
@@ -25,4 +25,13 @@ foreach ($itemsToPack as $item) {
     system("cp -r {$item} dist/monmap/{$item}");
 }
 
+// Set version in files to current git revision
+$gitRevision = trim(shell_exec('git rev-parse HEAD'));
+foreach (['index.htm', 'help.htm'] as $filename) {
+    $fullFilename = "dist/monmap/{$filename}";
+    $contents = file_get_contents($fullFilename);
+    $contents = str_replace('$VERSION', $gitRevision, $contents);
+    file_put_contents($fullFilename, $contents);
+}
+
 system("tar -C dist -cvf {$resultFilename} monmap");
